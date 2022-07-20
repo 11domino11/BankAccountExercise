47519f39963a31d1c15fa6c5857aeb20ac15b657
Damion Russell <domino.russell@gmail.com>
2022-07-20 12:33:21 -0400

    initial commit

---
+++ BankEncapsulationExercise/BankAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulationExercise
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+
+        }
+        public BankAccount(double balance)
+        {
+            Balance = balance;
+        }
+        public BankAccount(double balance, double amount)
+        {
+            Balance = balance;
+            Amount = amount;
+        }
+
+        private double Balance { get; set; }
+        private double Amount { get; set; }
+        public double Deposit(double amount)
+        {
+            return Balance+= amount;
+        }
+        public double GetBalance()
+        {
+            return Balance;
+        }
+    }
+}
---
+++ BankEncapsulationExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulationExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount damionRussellAccount = new BankAccount(200.05);
+            Console.WriteLine(damionRussellAccount.GetBalance());
+            Console.WriteLine(damionRussellAccount.Deposit(22.1));
+            Console.WriteLine(damionRussellAccount.GetBalance());
+            Console.ReadLine();
+        }
+    }
+}
